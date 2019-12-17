c8622c31d61e54793bc5ca4d35dedfcff818c49b
astrapi69 <asterios.raptis@gmx.net>
2019-12-17 17:10:27 +0100

    added slashes to path

--- src/main/java/de/alpharogroup/configuration/WebSecurityConfig.java
+++ src/main/java/de/alpharogroup/configuration/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
 			.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("authenticate", "hello").permitAll().
+				.authorizeRequests().antMatchers("/authenticate", "/hello").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
