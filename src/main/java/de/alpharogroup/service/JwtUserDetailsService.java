f326d640ed97834cf3856374e4fc1a0dee17045f
astrapi69 <asterios.raptis@gmx.net>
2019-12-17 14:16:12 +0100

    initial version

---
+++ src/main/java/de/alpharogroup/service/JwtUserDetailsService.java
@@ -0,0 +1,23 @@
+package de.alpharogroup.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		if ("topsecret".equals(username)) {
+			return new User("topsecret", "p2s5v8y/B?D(G+KbPeShVmYq3t6w9z$C&F)H@McQfTjWnZr4u7x!A%D*G-KaNdRg",
+					new ArrayList<>());
+		} else {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+	}
+}
